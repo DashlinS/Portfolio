9e47632cb8cb50c886272433d7026880bd2ad06a
dashlin <dashlin.sermeil@gmail.com>
2023-11-28 20:09:53 -0500

    nightmode and css styling view

--- js/scripts.js
+++ js/scripts.js
@@ -6,24 +6,55 @@
 //
 // Scripts
 //
+const headings = document.querySelectorAll('h1, h2, h3, h5');
+const subHeading = document.querySelectorAll('.subheading');
+const btnToggle = document.querySelector('.btn-toggle');
+let theme;
 
-//Night Mode
-document.querySelector('.btn-toggle').addEventListener('click', () => {
-  const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+//Save to Night-mode preference to localStorage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme == 'dark') {
+  darkTheme(headings, subHeading);
+}
+
+//Change theme event
+btnToggle.addEventListener('click', () => {
   if (!document.body.className.includes('dark-mode')) {
-    document.body.classList.toggle('dark-mode');
-    headings.forEach((heading) => {
-      heading.style.color = 'white';
-    });
+    darkTheme(headings, subHeading);
+    theme = 'dark';
   } else {
-    document.body.classList.remove('dark-mode');
-    headings.forEach((heading) => {
-      heading.style.color = '#343a40';
-    });
+    lightTheme(headings, subHeading);
+    theme = 'light';
   }
+
+  localStorage.setItem('theme', theme);
 });
 
-//Save to Nightmode preference to localStorage
+//Dark theme
+function darkTheme(headings, subHeading) {
+  document.body.classList.toggle('dark-mode');
+
+  headings.forEach((heading) => {
+    heading.style.color = 'white';
+  });
+
+  subHeading.forEach((heading) => {
+    heading.classList.add('subheadingColor');
+  });
+}
+
+//Light theme
+function lightTheme(headings, subHeading) {
+  document.body.classList.remove('dark-mode');
+
+  headings.forEach((heading) => {
+    heading.style.color = '#343a40';
+  });
+
+  subHeading.forEach((heading) => {
+    heading.classList.remove('subheadingColor');
+  });
+}
 
 window.addEventListener('DOMContentLoaded', (event) => {
   // Activate Bootstrap scrollspy on the main nav element
